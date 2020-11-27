105a890bccd75791f5b13c51413a6cd1cd5d55c7
CoderMan-X <1322281885@qq.com>
2020-11-27 18:53:44 +0800

    master

--- Project1/supermarketone/src/components/content/MainTabBar.vue
+++ Project1/supermarketone/src/components/content/MainTabBar.vue
@@ -2,23 +2,23 @@
   <div id="main-tab-bar">
     <tab-bar>
       <tab-bar-item path="/home">
-        <img slot="item-img-click" src="~assets/img/homeClick.png" alt="" />
-        <img slot="item-img" src="~assets/img/home.png" alt="" />
+        <img slot="item-img-click" src="~assets/img/homeClick.png" />
+        <img slot="item-img" src="~assets/img/home.png"  />
         <div slot="item-context">首页</div>
       </tab-bar-item>
       <tab-bar-item path="/categories">
-        <img slot="item-img-click" src="~assets/img/classifyClick.png" alt="" />
-        <img slot="item-img" src="~assets/img/classify.png" alt="" />
+        <img slot="item-img-click" src="~assets/img/classifyClick.png"  />
+        <img slot="item-img" src="~assets/img/classify.png" />
         <div slot="item-context">分类</div>
       </tab-bar-item>
       <tab-bar-item path="/shop">
-        <img slot="item-img-click" src="~assets/img/shoppingClick.png" alt="" />
-        <img slot="item-img" src="~assets/img/shopping.png" alt="" />
+        <img slot="item-img-click" src="~assets/img/shoppingClick.png" />
+        <img slot="item-img" src="~assets/img/shopping.png" />
         <div slot="item-context">购物车</div>
       </tab-bar-item>
       <tab-bar-item path="/my">
-        <img slot="item-img-click" src="~assets/img/myClick.png" alt="" />
-        <img slot="item-img" src="~assets/img/my.png" alt="" />
+        <img slot="item-img-click" src="~assets/img/myClick.png" />
+        <img slot="item-img" src="~assets/img/my.png" />
         <div slot="item-context">我的</div>
       </tab-bar-item>
     </tab-bar>
--- Project1/supermarketone/src/router/index.js
+++ Project1/supermarketone/src/router/index.js
@@ -3,31 +3,43 @@ import Vue from "vue"
 
 Vue.use(VueRouter)
 
-const Home = ()=>import("../views/home/Home")
-const Categories = ()=>import("../views/categories/Categories")
-const My = ()=>import("../views/my/My")
-const Shop = ()=>import("../views/shop/Shop")
+const Home = () => import("../views/home/Home")
+const Categories = () => import("../views/categories/Categories")
+const My = () => import("../views/my/My")
+const Shop = () => import("../views/shop/Shop")
 
 const routes = [
   {
-    path:"",
-    redirect: "/home"
+    path: "",
+    redirect: "/home",
   },
   {
-    path:"/home",
-    component:Home
+    path: "/home",
+    component: Home,
+    meta: {
+      title: "首页"
+    }
   },
   {
-    path:"/categories",
-    component:Categories
+    path: "/categories",
+    component: Categories,
+    meta: {
+      title: "分类"
+    }
   },
   {
-    path:"/my",
-    component:My
+    path: "/my",
+    component: My,
+    meta: {
+      title: "我的"
+    }
   },
   {
-    path:"/shop",
-    component:Shop
+    path: "/shop",
+    component: Shop,
+    meta: {
+      title: "购物车"
+    }
   },
 ]
 
@@ -35,6 +47,15 @@ const routes = [
 const router = new VueRouter({
   routes,
   mode: "history"
+})
+
+router.beforeEach((to, from, next) => {
+  //不用matched的话，初始页面中的meta是空值，无法找到对应的title
+  //用matched的原因是为了在开始的初始页面中找到title值
+  document.title = to.matched[0].meta.title
+  console.log(to);
+  console.log("+++++++++++++++");
+  next() 
 })
 
 export default router
